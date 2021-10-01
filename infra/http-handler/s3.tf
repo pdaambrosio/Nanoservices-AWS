dbbcabec452932a47e4633707d6394403130a821
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-10-01 16:12:09 -0300

    encryption and clean serverless.yml

--- infra/http-handler/s3.tf
+++ infra/http-handler/s3.tf
@@ -1,7 +1,19 @@
+resource "aws_kms_key" "bucket-handler-key" {
+   deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "bucket-handler-images" {
     bucket = "${var.prefix}-images"
     acl = "private"
     tags = var.tags
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          kms_master_key_id = aws_kms_key.bucket-handler-key.arn
+          sse_algorithm = "aws:kms"
+        }
+      }
+    }
 }
 
 resource "aws_s3_bucket_public_access_block" "block_public_access" {
