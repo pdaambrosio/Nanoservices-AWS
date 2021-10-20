eda942cf1da7c4e4e806867380e833b67d763d15
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-10-20 20:19:28 -0300

    lambda dynamoDB

--- http-handler/handler.js
+++ http-handler/handler.js
@@ -1,7 +1,10 @@
 const s3Service = require('./services/s3Service.js');
+const dynamodbService = require('./services/dynamodbService');
 
 module.exports.upload = async (event) => {
   const result = await s3Service.upload(event.body);
+  await dynamodbService.put(result);
+
   return {
     statusCode: 201,
     body: JSON.stringify(result),
