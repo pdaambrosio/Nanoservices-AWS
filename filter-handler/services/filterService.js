1f0366b68b1d7db4cc4114f7e38d4cbe2e47cbab
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-02 17:11:53 -0300

    filter send message to queue

--- filter-handler/services/filterService.js
+++ filter-handler/services/filterService.js
@@ -1,5 +1,6 @@
 const jimp = require('jimp');
 const s3Service = require('./s3Service');
+const sqsService = require('./sqsService');
 
 module.exports.filter = async (event) => {
     const s3Info = JSON.parse(event.Records[0].Sns.Message);
@@ -8,5 +9,8 @@ module.exports.filter = async (event) => {
     const objectS3 = await s3Service.getObject(bucket, key);
     const image = await jimp.read(objectS3);
     const buffer = await image.grayscale().quality(80).getBufferAsync(jimp.MIME_JPEG);
-    await s3Service.putObject(buffer, key);
+    const filterData = await s3Service.putObject(buffer, key);
+    filterData.eventType = 'FILTER_EVENT'
+
+    await sqsService.putMessage(filterData)
 }
