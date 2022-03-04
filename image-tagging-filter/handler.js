93c40b8e2bdabc68c981fc42e903de1502346d19
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-04 14:41:22 -0300

    image-tagging send message to queue

--- image-tagging-filter/handler.js
+++ image-tagging-filter/handler.js
@@ -1,10 +1,18 @@
-const rekognitionService = require("./services/rekognitionService.js");
+const rekognitionService = require("./services/rekognitionService");
+const sqsService = require("./services/sqsService");
 
 module.exports.tag = async (event) => {
     const s3Info = JSON.parse(event.Records[0].Sns.Message);
     const bucket = s3Info.Records[0].s3.bucket.name;
     const key = s3Info.Records[0].s3.object.key;
-    const data = await rekognitionService.data(bucket, key);
+    const labels = await rekognitionService.data(bucket, key);
+    
+    const item = {};
+    item.key = key;
+    item.labels = labels;
+    item.eventType = 'TAG_EVENT';
 
-    console.log(data.Labels);
+    await sqsService.putMessage(item);
+
+    return {event};
 };
