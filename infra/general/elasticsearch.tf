40c2afccfbe7bbece4ee3125fa2462d8bb778816
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-13 14:16:58 -0300

    fix: uncommented elasticsearch.tf

--- infra/general/elasticsearch.tf
+++ infra/general/elasticsearch.tf
@@ -1,27 +1,27 @@
-# resource "aws_elasticsearch_domain" "elasticsearch-images" {
-#     domain_name = "elk-images"
-#     elasticsearch_version = "7.10"
+resource "aws_elasticsearch_domain" "elasticsearch-images" {
+    domain_name = "elk-images"
+    elasticsearch_version = "7.10"
 
-#     cluster_config {
-#       instance_count  = 1
-#       instance_type   = "t2.small.elasticsearch"
-#     }
+    cluster_config {
+      instance_count  = 1
+      instance_type   = "t2.small.elasticsearch"
+    }
 
-#     vpc_options {
-#       subnet_ids          = [module.vpc.private_subnets[0]]
-#       security_group_ids  = [module.security-group.security_group_id]
-#     }
+    vpc_options {
+      subnet_ids          = [module.vpc.private_subnets[0]]
+      security_group_ids  = [module.security-group.security_group_id]
+    }
 
-#     ebs_options {
-#       ebs_enabled = true
-#       volume_size = 10
-#       volume_type = "gp2"
-#     }
+    ebs_options {
+      ebs_enabled = true
+      volume_size = 10
+      volume_type = "gp2"
+    }
 
-#     tags = var.tags
-# }
+    tags = var.tags
+}
 
-# resource "aws_iam_service_linked_role" "elasticsearch-images-linked-role" {
-#     aws_service_name = "es.amazonaws.com"
-#     description      = "Allows Amazon ES to manage AWS resources for a domain."
-# }
+resource "aws_iam_service_linked_role" "elasticsearch-images-linked-role" {
+    aws_service_name = "es.amazonaws.com"
+    description      = "Allows Amazon ES to manage AWS resources for a domain."
+}
