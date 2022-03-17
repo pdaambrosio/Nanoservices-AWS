03942e1db4056c83a45f4f95547e1740f561e835
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-17 14:48:42 -0300

    feat: dynamodb service

--- image-handler-consumer/services/elasticsearchService.js
+++ image-handler-consumer/services/elasticsearchService.js
@@ -1,14 +1,14 @@
 const { Client } = require('@elastic/elasticsearch');
 
 const Client = new Client({
-  apiVersion: "7.10",
-  host: "http://localhost:9200"
+  apiVersion: '7.10',
+  host: 'http://localhost:9200'
 });
 
 module.exports.index = async (document) => {
     await Client.index({
-        index: "images",
-        type: "object",
+        index: 'images',
+        type: 'object',
         body: document
     })
 };
