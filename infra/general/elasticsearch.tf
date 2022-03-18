7ab137713ab0600b0968820c77cb981680aab61a
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-18 16:18:13 -0300

    fix: Weak SSL/TLS protocols should not be used

--- infra/general/elasticsearch.tf
+++ infra/general/elasticsearch.tf
@@ -1,6 +1,10 @@
 resource "aws_elasticsearch_domain" "elasticsearch-images" {
   domain_name           = "elk-images"
   elasticsearch_version = "7.10"
+  domain_endpoint_options {
+    enforce_https       = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
+  }
 
   cluster_config {
     instance_count = 1
