411b825434981280c6f41e34f2fe86e535508d93
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-19 14:44:01 -0300

    fix: Client is already defined.

--- image-handler-consumer/services/elasticsearchService.js
+++ image-handler-consumer/services/elasticsearchService.js
@@ -1,12 +1,12 @@
 const { Client } = require('@elastic/elasticsearch');
 
-const Client = new Client({
+const elastic = new Client({
   apiVersion: '7.10',
   host: 'http://localhost:9200'
 });
 
 module.exports.index = async (document) => {
-    await Client.index({
+    await elastic.index({
         index: 'images',
         type: 'object',
         body: document
