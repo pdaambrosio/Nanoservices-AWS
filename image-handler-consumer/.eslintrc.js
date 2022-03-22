71b2918337d5abacc40cd51cf21491e2253c0df7
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-21 23:04:56 -0300

    test: add eslint

---
+++ image-handler-consumer/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "root": true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+  },
+};
