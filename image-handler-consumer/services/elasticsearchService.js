794e078eb07c12f098da68ca6ba1ef59a01a750f
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-21 23:29:03 -0300

    fix: prettier singleQuote and spaces

--- image-handler-consumer/services/elasticsearchService.js
+++ image-handler-consumer/services/elasticsearchService.js
@@ -1,14 +1,14 @@
-const { Client } = require('@elastic/elasticsearch');
+const { Client } = require("@elastic/elasticsearch");
 
 const elastic = new Client({
-  apiVersion: '7.10',
-  host: 'http://localhost:9200'
+  apiVersion: "7.10",
+  host: "http://localhost:9200",
 });
 
 module.exports.index = async (document) => {
-    await elastic.index({
-        index: 'images',
-        type: 'object',
-        body: document
-    })
+  await elastic.index({
+    index: "images",
+    type: "object",
+    body: document,
+  });
 };
