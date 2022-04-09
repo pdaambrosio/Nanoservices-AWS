146c4013520cc8535c7eb8f630a72de45800735c
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-04-08 23:34:08 -0300

    refactor: elasticsearch service

--- http-handler/services/elasticsearchService.js
+++ http-handler/services/elasticsearchService.js
@@ -1,20 +1,31 @@
 const elasticsearch = require("@elastic/elasticsearch");
 const AWS = require("aws-sdk");
-const ssm = new AWS.SSM({ region: "us-east-1" });
 
-const getElasticSearchEndpoint = async () => {
-  const endpoint = await ssm
-    .getParameter({
-      Name: "/general/elasticsearch-endpoint",
-      WithDecryption: true,
-    })
-    .promise();
-  return endpoint.Parameter.Value;
+const getEndPoint = async (address) => {
+  return new Promise((resolve, reject) => {
+    const ssmClient = new AWS.SSM({
+      apiVersion: "2014-11-06",
+      region: "us-east-1",
+    });
+
+    ssmClient.getParameter(
+      {
+        Name: address,
+        WithDecryption: true,
+      },
+      (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data.Parameter.Value);
+      }
+    );
+  });
 };
 
 const client = new elasticsearch.Client({
   apiVersion: "7.10",
-  node: getElasticSearchEndpoint(),
+  node: await getEndPoint("/general/elasticsearch-endpoint"),
 });
 
 module.exports.search = async (query) => {
