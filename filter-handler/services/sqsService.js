6e11c1596d01527537d671091116b16227339d84
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-05-14 16:32:45 -0300

    refactor: included ssm service

--- filter-handler/services/sqsService.js
+++ filter-handler/services/sqsService.js
@@ -1,13 +1,18 @@
 const AWS = require("aws-sdk");
-
+const ssm = require("../services/ssmService");
 const sqs = new AWS.SQS({ apiVersion: "2012-11-05", region: "us-east-1" });
 
-module.exports.putMessage = (message) => {
+const ssmGetParameter = async (address) => {
+  const response = await ssm.getParameter(address);
+  return response;
+};
+
+module.exports.putMessage = async (message) => {
+  const sqsUrl = await ssmGetParameter("/general/sqs-url");
   return new Promise((resolve, reject) => {
     sqs.sendMessage(
       {
-        QueueUrl:
-          "https://sqs.us-east-1.amazonaws.com/489716104000/post-processing-image-queue",
+        QueueUrl: sqsUrl,
         MessageBody: JSON.stringify(message),
       },
       (err, data) => {
